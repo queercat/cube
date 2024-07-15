e634166bc261db3c31e9e220abf88409e99e11d7
Or-If <143971808+Or-If@users.noreply.github.com>
2024-07-14 19:45:15 -0700

    added SQLite database

--- backend/controllers/CubeController.cs
+++ backend/controllers/CubeController.cs
@@ -2,7 +2,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace backend.controllers;
 
-public class CubeController : Controller
+[ApiController]
+[Route("/data")]
+public class CubeController : ControllerBase
 {
-    
+    private string connectionString = "Data Source=c:../database/cube.db;Version=3;";
+
+
+
 }
+
+
+
+
