6e837539f7d4b6f5c0db20a51135d23c4f87c725
Or-If <143971808+Or-If@users.noreply.github.com>
2024-07-19 22:06:49 -0700

    Finished basic outline of DB

--- backend/entities/Archtype.cs
+++ backend/entities/Archtype.cs
@@ -2,5 +2,8 @@ namespace backend.entities;
 
 public class Archtype
 {
+    public int ArchetypeId { get; set; }
+    
+    public List<CardInArchetype> CardsInArchetype { get; set; }
     
 }
--- backend/entities/Card.cs
+++ backend/entities/Card.cs
@@ -2,5 +2,8 @@ namespace backend.entities;
 
 public class Card
 {
+    public int CardId { get; set; }
     
+    public int OracleId { get; set; } // If we wanna have scryfall get the info everytime
+    public string Image { get; set; } // Possible using this if we download and host a library of playable cards
 }
