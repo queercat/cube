9ee6bf43769cb0d67a06471357a5108198a54cb3
Or-If <143971808+Or-If@users.noreply.github.com>
2024-07-21 16:05:28 -0700

    Updated Card table & migration

--- backend/entities/Card.cs
+++ backend/entities/Card.cs
@@ -3,7 +3,8 @@ namespace backend.entities;
 public class Card
 {
     public int CardId { get; set; }
+    public required string name { get; set; }
     
-   // public int OracleId { get; set; } // If we wanna have scryfall get the info everytime
+    public int OracleId { get; set; } // If we wanna have scryfall get the info everytime
    // public string Image { get; set; } // Possible using this if we download and host a library of playable cards
 }
