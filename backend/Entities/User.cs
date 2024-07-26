1eae8a15d129038f0c386b02cc142a7ad4c43d08
orianna-20i <orianna@twentyideas.com>
2024-07-26 13:58:33 -0700

    #7 - Updated User Table & Updated Migration

--- backend/Entities/User.cs
+++ backend/Entities/User.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
 using backend.entities;
 
 namespace backend.entities;
@@ -8,6 +9,11 @@ public class User
 {
     public required int UserId { get; set; }
     public required string Name { get; set; }
+    
+    public required string Username { get; set; }
+    public required Guid Password { get; set; }
+    public required string EmailAddress { get; set; }
+    
 
     public required ICollection<Cube> Cubes { get; set; }
 }
