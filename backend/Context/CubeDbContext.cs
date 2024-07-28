a8244020ec8a944a7d126bf300acb53c73234e4d
May Tusek <tusek.may@gmail.com>
2024-07-28 15:02:51 -0700

    Removes lower case version.

--- backend/Context/CubeDbContext.cs
+++ backend/Context/CubeDbContext.cs
@@ -3,18 +3,22 @@ using Microsoft.EntityFrameworkCore;
 
 namespace backend.context;
 
-public class CubeDbContext(DbContextOptions<CubeDbContext> options) : DbContext(options)
+public class CubeDbContext : DbContext
 {
     
-    public virtual DbSet<User> Users { get; set; }
-    public virtual DbSet<UserSession> UserSessions { get; set; }
+    public DbSet<UserAuth> UserAuths { get; set; } // this feels like it shouldn't belong here.
     
-    public virtual DbSet<Cube> Cubes { get; set; }
-    public virtual DbSet<Card> Cards { get; set; }
-    public virtual DbSet<Archetype> Archetypes { get; set; }
-    
-    public virtual DbSet<CardInArchetype> CardsInArchetype { get; set; }
-    public virtual DbSet<CardInCube> CardsInCube { get; set; }
+    public DbSet<Cube> Cubes { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Card> Cards { get; set; }
+    public DbSet<Archetype> Archetypes { get; set; }
+    public DbSet<CardInArchetype> CardsInArchetype { get; set; }
+    public DbSet<CardInCube> CardsInCube { get; set; }
+
+    public CubeDbContext( DbContextOptions<CubeDbContext> options) : base( options )
+    {
+        
+    }
     
     
 }
